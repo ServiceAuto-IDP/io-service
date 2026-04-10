4fc33e7b23f9c494ab521b796ae87554f1a8f0e6
Gabriel Nitu <gabrielnitu149@gmail.com>
2026-04-11 01:43:20 +0300

    Empty spring project

---
+++ src/main/java/com/serviceauto/io_service/IoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.serviceauto.io_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serviceauto/io_service/IoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceauto.io_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
